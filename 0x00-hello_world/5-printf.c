08e5c9c419fab7b1cfc1bff1c257b81a5cd412a9
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-08 22:53:12 -0700

    creating c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+
+
+
 /**
- * main - entry into the program
+ *  * main - entry into the program
  *
- * Return: prints the message
- *  "with proper grammar, but the outcome is a piece of art"
- *  to the screen
- */
+ *   *
+ *
+ *    * Return: prints the message
+ *
+ *     *  "with proper grammar, but the outcome is a piece of art"
+ *
+ *      *  to the *r      */
+
 int main(void)
+
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);  
-}
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
