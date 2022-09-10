e272d220f21b39691b9d335bab235a73ee12af86
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-10 12:39:13 -0700

    first c variable code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ scanf("%d", &n);
 
 if (n > 0)
 {
-printf(n, "%d is positive\n");
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf(n, "%d is zero\n");
+printf("%d is zero\n", n);
 }
 else if (n < 0)
 {
-printf(n,"%d is negative\n");
+printf("%d is negative\n", n);
 }
 
 /* your code goes there */
