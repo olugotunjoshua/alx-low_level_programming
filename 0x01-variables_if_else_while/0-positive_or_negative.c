4c97c3c4bc540c30294ebc4221cd516d21835465
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-10 13:03:35 -0700

    first c variable code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,12 @@
 #include <time.h>
 /* more headers goes there */
 
-
-/* betty style doc for function main goes there */
+/**
+ *main-Entry point
+ *
+ *Return: Always 0 (Success)
+ * betty style doc for function main goes there
+ */
 
 
 int main(void)
