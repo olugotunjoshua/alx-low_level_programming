65fc44ca1b10c3c8bfc66e6168e70957208b3fcb
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-10 16:37:46 -0700

    last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char x = 'a';
+
+while (c <= 'z')
+{
+
+putchar(x);
+
+x++
+}
+putchar("\n");
+
+return (0);
+
+}
