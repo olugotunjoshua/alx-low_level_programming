881393e54a07a7db1032cceb797267d3bdafd2cb
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-11 15:08:36 -0700

    exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Main- Entry point
+ *
+ *Return:Always (0)
+ */
+
+int main(void)
+{
+char low = 'a';
+
+for (low; low <= 'z'; low++)
+{
+if (low != 'e' && low != 'q')
+putchar (low);
+}
+putchar ('\n');
+
+return (0);
+}
