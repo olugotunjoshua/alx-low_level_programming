91adb75853a55a65c5ac5e4f11e45dfc940936e9
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-12 02:13:50 -0700

    hello world

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-Entry point
+ *Return: 0
+ */
+int main(void)
+{
+char low  = 'z';
+for (low = 'z'; low <= 'a'; low++)
+{
+putchar(low);
+}
+putchar ('\n');
+return (0);
+}
