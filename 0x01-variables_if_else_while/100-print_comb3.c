56768a434d31c295d8aeb554fbbcd4cab4f58e2c
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-13 05:47:43 -0700

    two digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- entry point
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+int n;
+for (n = 11; n <= 34; n++)
+{
+putchar(n);
+if (n == 34)
+{
+break;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
