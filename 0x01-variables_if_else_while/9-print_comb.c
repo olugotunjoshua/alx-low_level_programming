278cde542cf6670b0f3617555341701d0dc5e412
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-13 03:31:11 -0700

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- Entry point
+ *
+ *Return: always (0)
+ */
+
+int main(void)
+{
+int n;
+
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+if (n != '9')
+{
+putchar (',');
+putchar(' ');
+}
+}
+putchar ('\n');
+return (0);
+}
