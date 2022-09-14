d9dc381ccb18c57bc029b3265ac982b34a780843
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-14 14:22:47 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,14 @@
  */
 int main(void)
 {
-printf("_putchar");
-printf("\n");
-
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+-putchar('r');
+_putchar('\n');
 return (0);
 }
