eb5084c723e5dfd62aa31067d10701a470187fc6
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-15 09:37:47 -0700

    hello

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,22 @@
 #include "main.h"
 /**
- *check code alphabet
- * 
- *Return: 0
- */
+*main-check code alphabet 
+*Return: 0
+*/
+void print_alphabet(void);
+
 
 int main(void)
 {
 print_alphabet();
 return (0);
+}
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,1 @@
 #include <stdio.h>
-/**
- *main-entry point
- * 
- *Return:0
- */
-
-void print_alphabet(void)
-{
-char x;
- for(x = 'a'; x <= 'z'; x++)
-{
-putchar(x);
-}
-putchar('\n');
-}
