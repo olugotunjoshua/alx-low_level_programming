ac0ddec14ef575532cce1f5de9df6af66ac1cab0
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-17 07:49:40 -0700

    pls work

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -30,9 +30,9 @@ int main(void)
 r = _islower('H');
 putchar(r + '0');
 r = _islower('o');
-_putchar(r + '0');
+putchar(r + '0');
 r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
+putchar(r + '0');
+putchar('\n');
 return (0);
 }
