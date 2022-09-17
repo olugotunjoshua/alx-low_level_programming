e29876585705f6360a10b51f78a5a0f0c917b539
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-17 07:12:00 -0700

    debuggers

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <time.h>
 
 /**
  * main - tests function that prints if integer is positive or negative
@@ -8,8 +9,9 @@
 int main(void)
 {
 int i;
-
+ srand(time(0));
+ n = rand() - RAND_MAX / 2;
 i = 98;
 positive_or_negative(i);
-return (0);
+ return (0);
 }
