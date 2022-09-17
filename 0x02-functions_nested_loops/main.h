2882eeaa04a76ec22f9e9ad98390a54f62cb0937
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-17 05:37:13 -0700

    welcome onboard

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-
-int main(void)
-{
-int r;
-
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
-return (0);
-}
