a13a60bce828ee48b9ff9e1c21c11696eeb4b6fa
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-17 05:28:40 -0700

    welcome onboard

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,17 @@
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+
+int main(void)
+{
+int r;
+
+r = _islower('H');
+_putchar(r + '0');
+r = _islower('o');
+_putchar(r + '0');
+r = _islower(108);
+_putchar(r + '0');
+_putchar('\n');
+return (0);
+}
