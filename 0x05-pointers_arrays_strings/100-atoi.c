08b7c96cf3f31705aa02490b3a2c9f27485f1ddd
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-19 22:22:59 -0700

    pointer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,24 @@
 #include "main.h"
 /**
- * _atoi - converts 
- * @s : character to check
- * Return:0 if there are number
+ *_atoi -converts
+ *@s : character to check
+ *Return:0 if there are number
  */
 
 int _ atoi(char *s)
 {
 	int sn = 1;
 	unsigned int n = 0;
-	do{
-		if (*s == '-')
+do {
+	if (*s == '-')
+	sn *= -1;
 
-sn *= -1;
+	else if (*s >= '0' && *s <= '9')
+	n = (n * 10) + (*s - '0');
+	else if (n > 0)
+	break;
 
-	       else if (*s >= '0' && *s <= '9')
 
-		n = (n * 10) + (*s - '0');
-
-		else if (n > 0)
-
-		break;
-
-	} while (*s++);
-
-		return (n * sn);
+} while (*s++);
+return (n * sn);
 }
