042a5a28b81a2ae73a5823493519b3d7d32fc705
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-19 21:37:26 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/** 
+ * reset_to_98 -takes pointer to upfate
+ * @n: character to check
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/**
+ * main- check code 
+ * Return: 0
+ */
+
+void reset_to_98(int *n);
