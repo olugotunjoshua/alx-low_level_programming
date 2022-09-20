834b6064d43e66123bd39abaeab231d887cb69b6
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-19 22:45:34 -0700

    swap integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int- code for swapping two integers
+ * @a: check code
+ * @b: check code
+ */
+
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a;
+*a = *b;
+*b = c
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@
 
 void reset_to_98(int *n);
 int _atoi(char *s);
+void swap_int(int *a,int *b);
