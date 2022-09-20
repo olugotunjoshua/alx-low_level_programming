0c7569f4c4fd3f2c28394108603e145b10fcc89e
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-20 13:35:38 -0700

    hello

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * _strlen - check code
- * @s - character to check
+ * @s: - character to check
  * Return: length
  */
 
 int _strlen(char *s)
 {
-	size_t length = 0;
-	while (*s++)
-		length++;
-	return (length);
+size_t length = 0;
+while (*s++)
+	length++;
+return (length);
 }
