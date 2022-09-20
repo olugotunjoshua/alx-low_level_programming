e1acbab5193eb3936d6069e8603cb5ef4b62a476
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-20 15:15:32 -0700

    hello

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,11 +5,11 @@
  */
 void print_rev(char *s)
 {
-	int a;
-	a = 0;
-	while (s[a] != '\0')
-		a++;
-	for (a -= 1; a >= 0; a--)
-		putchar(s[a]);
-	putchar('\n');
+int a;
+a = 0;
+while (s[a] != '\0')
+	a++;
+for (a -= 1; a >= 0; a--)
+	putchar(s[a]);
+putchar('\n');
 }
