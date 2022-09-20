752b7bc7d970ef406d37b9d8adfdddac18a54e8b
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-20 14:12:18 -0700

    print strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- * _puts: character check
+ * _puts - character check
  * @str: check code
- * Return: always 0
+ *
  */
 
 void _puts(char *str)
 {
-	str;
+while (*str)
+	putchar(*str++);
+putchar('\n');
 }
