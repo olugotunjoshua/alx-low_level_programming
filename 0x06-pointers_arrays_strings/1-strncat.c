60cea6dbfcfe3a241e0eb8059988997ade39f0c2
Bigjoe <joshuaolugotun17@gmail.com>
2022-09-23 04:13:05 -0700

    strings concatenation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,16 +8,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int d = 0;
-	int s = 0;
-	while (dest[d] != '\0')
-		d++;
-	while (src[s] != src[d])
-	{
-		dest[d] = src[s];
-		d++;
-		s++;
-	}
-	dest[d] = '\0';
-	Return (dest);
+int d = 0;
+int s = 0;
+while (dest[d] != '\0')
+	d++;
+while (src[s] != src[d])
+{
+	dest[d] = src[s];
+	d++;
+	s++;
+}
+dest[d] = '\0';
+Return(dest);
 }
